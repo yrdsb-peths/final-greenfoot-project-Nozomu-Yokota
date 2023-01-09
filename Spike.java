5cf23fdbd09e85c29242033b390176e8c81d7284
Nozomu-Yokota <349888354@gapps.yrdsb.ca>
2023-01-09 14:01:51 -0500

    ass spike in the game

---
+++ Spike.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spike here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spike extends Actor
+{
+    int gravity = 2;
+    /**
+     * Act - do whatever the Spike wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        int x = getX();
+        int y = getY();
+        setLocation(x, y + gravity);
+    }
+}
