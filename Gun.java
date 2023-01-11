b2e49b5ce70fcf15f2bb078d5443a59db00b3829
Nozomu-Yokota <349888354@gapps.yrdsb.ca>
2023-01-11 14:01:32 -0500

    change the size of the image

--- Gun.java
+++ Gun.java
@@ -8,10 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Gun extends Actor
 {
+    GreenfootImage g = new GreenfootImage("images/images-removebg-preview.png");
     /**
      * Act - do whatever the Gun wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Gun()
+    {
+        g.scale(100, 100);
+        setImage(g);
+    }
     public void act()
     {
         // Add your action code here.
